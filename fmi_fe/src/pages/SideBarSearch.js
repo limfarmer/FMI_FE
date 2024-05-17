fe3ddb89c50cd2ff4bb27c8d8773f9c4af08aa21
Lim JeongHu <dlaeocjf94@naver.com>
2024-05-17 19:05:55 +0900

    축구 정보 받아오는 부분만 따로 컴포넌트 파일로 만들었습니다

--- fmi_fe/src/pages/SideBarSearch.js
+++ fmi_fe/src/pages/SideBarSearch.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
+import FootballApiData from "./FootballData";
 
 /**
  *
@@ -7,55 +7,12 @@ import axios from "axios";
  * 사이드바의 검색기능 컴포넌트
  */
 const SideBarSearch = () => {
-  const [teamsArray, setTeamsArray] = useState([]); // api에서 받아온 데이터를 담는 객체
-  const [playerArray, setPlayerArray] = useState([]); // api에서 받아온 데이터를 담는 객체
-  const [loading, setLoading] = useState(true); // 정보 나오기 전까지의 로딩 상태 관리 loading창 구현할 경우 활용 예정
   const [inputValue, setInputValue] = useState("");
+  const { teamsArray, playerArray, loading } = FootballApiData(inputValue);
 
-  // **** 팀 검색 로직 ****
-
-  /**
-   *
-   * @param {*} api에서 받아오는 JSON 데이터
-   * @returns 팀과 팀 로고만 배열에 담음
-   */
-  const transformData = (data) => {
-    return data.teams.map((team) => {
-      return {
-        strTeam: team.strTeam,
-        strTeamBadge: team.strTeamBadge,
-      };
-    });
-  };
-
-  /**
-   * 축구 api의 정보를 가져와서 TeamsArray에 담는 hook
-   */
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get(
-          "https://www.thesportsdb.com/api/v1/json/3/search_all_teams.php?l=Spanish%20La%20Liga"
-        );
-        const transformedData = transformData(response.data);
-        console.log("!");
-        setTeamsArray(transformedData);
-        setLoading(false);
-      } catch (e) {
-        setLoading(false);
-      }
-    };
-    fetchData();
-  }, []);
-
-  /**
-   *
-   * @param {*} input창에 입력값을 받는 event 핸들러
-   */
   const handleSearchChange = (event) => {
     setInputValue(event.target.value);
   };
-
   /**
    * filter로 공백을 제거한 검색값 비교하는 함수
    */
@@ -75,43 +32,6 @@ const SideBarSearch = () => {
      */
     return trimmedTeamName.includes(trimmedInputValue);
   });
-
-  // **** 여기서 부턴 선수 검색 로직 ****
-
-  /**
-   *
-   * @param {*} api에서 받아오는 JSON 데이터
-   * @returns 선수, 선수의 팀을 반환
-   */
-  const transformPlayerData = (data) => {
-    return data.player.map((player) => {
-      return {
-        strPlayer: player.strPlayer,
-        strTeam: player.strTeam,
-      };
-    });
-  };
-  /**
-   * 선수 데이터를 가지고 오는 api 불러오고
-   * input입력값에 따라 상태 관리를 하는 hook
-   */
-  useEffect(() => {
-    const fetchData = async () => {
-      if (inputValue.trim() === "") return;
-      try {
-        const response = await axios.get(
-          `https://www.thesportsdb.com/api/v1/json/3/searchplayers.php?p=${inputValue.trim()}`
-        );
-        console.log(response.data, "!");
-        const transformedData = transformPlayerData(response.data);
-        setPlayerArray(transformedData);
-        setLoading(false);
-      } catch (e) {
-        setLoading(false);
-      }
-    };
-    fetchData();
-  }, [inputValue]);
 
   /**
    * filter로 공백을 제거한 검색값 비교하는 함수
