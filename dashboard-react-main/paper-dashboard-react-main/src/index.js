f222065432aeaa4c72e64b5a2dbcc9e48dc41c47
Lim JeongHu <dlaeocjf94@naver.com>
2024-05-17 11:51:30 +0900

    index 수정후 push

--- dashboard-react-main/paper-dashboard-react-main/src/index.js
+++ dashboard-react-main/paper-dashboard-react-main/src/index.js
@@ -7,7 +7,6 @@ import "assets/scss/paper-dashboard.scss?v=1.3.0";
 import "assets/demo/demo.css";
 import "perfect-scrollbar/css/perfect-scrollbar.css";
 import AdminLayout from "layouts/Admin.js";
-import UserSideBar from "views/userSidebar/UserSidebar";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
@@ -16,7 +15,6 @@ root.render(
     <Routes>
       <Route path="/admin/*" element={<AdminLayout />} />
       <Route path="/" element={<Navigate to="/admin/dashboard" replace />} />
-      <Route path="/userSideBar" element={<UserSideBar />} />
     </Routes>
   </BrowserRouter>
 );
