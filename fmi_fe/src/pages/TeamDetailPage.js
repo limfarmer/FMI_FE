d8e7c3ef9a361c60f4195d0dba8b2adfe44b5f4f
user1 <98tmddyddl@gmail.com>
2024-05-21 18:15:23 +0900

    vs 완성, 상세내용추가

--- fmi_fe/src/pages/TeamDetailPage.js
+++ fmi_fe/src/pages/TeamDetailPage.js
@@ -7,7 +7,7 @@ const DetailStyle = styled.div`
   display: flex;
   height: 4620px;
   flex-direction: column;
-  color: blue;
+  color: black;
   > * {
     height: 924px;
     display: flex;
@@ -24,21 +24,24 @@ const DetailStyle = styled.div`
     flex-direction: column;
   }
   > :nth-child(2) {
-    background-color: white;
+    background-color: aliceblue;
   }
   > :nth-child(3) {
-    background-color: red;
+    background-color: aliceblue;
   }
   > :nth-child(4) {
     background-color: aliceblue;
   }
   > :nth-child(5) {
-    background-color: violet;
+    background-color: aliceblue;
+  }
+  li {
+    list-style-type: none;
   }
 `;
 const Logo = styled.div`
-  width: 200px;
-  height: 200px;
+  width: 300px;
+  height: 300px;
   background-image: ${({ image }) => `url(${image})`};
   background-size: cover;
 `;
@@ -48,7 +51,6 @@ const TeamDetailPage = () => {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(false);
   const [FollowTeam, setFollowTeam] = useState(null);
-  // const [bttTeamName, setBttTeamName] = useState("");
   const [versus, setVersus] = useState([]);
   const [awayTeams, setAwayTeams] = useState([]);
 
@@ -59,7 +61,6 @@ const TeamDetailPage = () => {
           "https://www.thesportsdb.com/api/v1/json/3/searchteams.php?t=" +
             teamName
         );
-
         setData(response.data.teams);
       } catch (error) {
         console.error("Error fetching data:", error);
@@ -74,11 +75,6 @@ const TeamDetailPage = () => {
     setFollowTeam(event.target.value);
   };
 
-  // const handleButtonClick = () => {
-  //   setBttTeamName(FollowTeam);
-  //   vs();
-  // };
-
   const transformData = (data) => {
     return data.teams.map((team) => {
       return {
@@ -86,13 +82,50 @@ const TeamDetailPage = () => {
       };
     });
   };
-  const versusData = (event) => {
-    return data.teams.map((team) => {
-      return {
-        strTeam: team.strTeam,
-      };
-    });
+
+  const versusData = (data) => {
+    if (!data || !data.event) {
+      return []; // 데이터가 없는 경우 빈 배열 반환
+    }
+
+    return data.event
+      .map((away) => {
+        if (away.strSeason === "2023-2024") {
+          return {
+            strEvent: away.strEvent,
+          };
+        } else {
+          return null;
+        }
+      })
+      .filter((item) => item !== null);
   };
+
+  useEffect(() => {
+    const vs = async () => {
+      try {
+        const awayTeamsResponse = await axios.get(
+          `https://www.thesportsdb.com/api/v1/json/3/search_all_teams.php?l=English%20Premier%20League`
+        );
+        const awayTeams = awayTeamsResponse.data.teams.map(
+          (team) => team.strTeam
+        );
+        const versusDataArray = await Promise.all(
+          awayTeams.map(async (awayTeam) => {
+            const response = await axios.get(
+              `https://www.thesportsdb.com/api/v1/json/3/searchevents.php?e=${teamName}_vs_${awayTeam}`
+            );
+            return versusData(response.data);
+          })
+        );
+        const flattenedVersusData = versusDataArray.flat();
+        setVersus(flattenedVersusData);
+      } catch (error) {
+        console.error("error", error);
+      }
+    };
+    vs();
+  }, [teamName]);
 
   useEffect(() => {
     /* awayteam받아오는api */
@@ -111,60 +144,36 @@ const TeamDetailPage = () => {
     };
     fetchData();
   }, []);
-  useEffect(() => {
-    const vs = async () => {
-      try {
-        // setVersus([]);
-        for (const awayTeam of awayTeams) {
-          if (awayTeams.strTeam !== teamName) {
-            const response = await axios.get(
-              `https://www.thesportsdb.com/api/v1/json/3/searchevents.php?e=${teamName}_vs_${awayTeam.strTeam}`
-            );
-            const versusData = transformData(response.data);
-            console.log(response.data, "결과");
-            setVersus(response.data);
-          }
-        }
-      } catch (error) {
-        console.error("error", error);
-      }
-    };
-    vs();
-  }, [teamName, awayTeams]);
 
   return (
     <DetailStyle>
       <div>
-        <input
-          type="text"
-          value={teamName}
-          onChange={handleInputChange}
-          placeholder="Enter team name"
-        />
-        {/* <button onClick={handleButtonClick}>Fetch Team Data</button> */}
-
         {data &&
           data.map((data, index) => (
             <div key={index}>
               <Logo image={data.strTeamBadge} />
+              <div>{data.strAlternate}</div>
             </div>
           ))}
       </div>
-
-      <div>{data[0] && data[0].strDescriptionEN} section2</div>
+      <div>{data[0] && data[0].strDescriptionEN}</div>
       <div>
-        <div>
-          <h2>Events</h2>
-          <ul>
-            {versus &&
-              versus.map((versus, index) => (
-                <li key={index}>{versus.strEvent}</li>
-              ))}
-          </ul>
-        </div>
+        <h2>경기일정</h2>
+        <ul>
+          {versus &&
+            [...new Set(versus.map((versus) => versus.strEvent))].map(
+              (uniqueEvent, index) => <li key={index}>{uniqueEvent}</li>
+            )}
+        </ul>
       </div>
-      <div>{data[0] && data[0].strDescriptionEN}</div>
-      <div>{data[0] && data[0].strDescriptionEN}</div>
+
+      <div>{data[0] && data[0].strStadiumDescription}</div>
+      <div>
+        <p>웹사이트: {data[0] && data[0].strWebsite}</p>
+        <div> 페이스북: {data[0] && data[0].strFacebook}</div>
+        <div> 트위터: {data[0] && data[0].strTwitter}</div>
+        <div> 인스타그램: {data[0] && data[0].strInstagram}</div>
+      </div>
     </DetailStyle>
   );
 };
